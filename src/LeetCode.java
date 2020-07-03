f20dc9e50d6a46cf36a7fa2baa68b2d49dacae61
Manish Rai <rai00007@Manishs-MBP.hsd1.wa.comcast.net>
2020-07-02 23:41:10 -0700

    1. palindrome 2. reversing a number

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,6 +1,5 @@
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
 class LeetCode {
+  // 1. Given an array of integers, return indices of the two numbers such that they add up to a specific target.
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -10,5 +9,32 @@ class LeetCode {
             }
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    // 2. Given a 32-bit signed integer, reverse digits of an integer.
+    public int reverse(int x) {
+        int rev = 0;
+        while(x!=0){
+            int pop = x%10; //pop operation
+            x = x/10;
+            // identifying overflows
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev*10+pop; // push operation
+        }
+        return rev;
+    }
+
+    // 3. Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber/10;
     }
 }
