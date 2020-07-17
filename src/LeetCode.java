48ac549535e14fb25f6f1ea235e01dd33914955e
Manish-rai21bit <manish.rai21bit@gmail.com>
2020-07-16 23:14:38 -0700

    Pascal's Triangle
    Correcting the author

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -62,4 +62,33 @@ class LeetCode {
         callBFS(grid, i, j+1);
         callBFS(grid, i, j-1);
     }
+
+    // 5. Pascal's Triangle
+    public List<List<Integer>> generate(int numRows) {
+       List<List<Integer>> tri = new ArrayList<>();
+       if (numRows==0) return tri;
+
+       // Then the 2nd row is alway 1
+        tri.add(new ArrayList<>());
+        tri.get(0).add(1);
+
+       for(int i=1; i<numRows; i++){
+           List<Integer> prev_row = tri.get(i-1);
+           List<Integer> row = new ArrayList<>();
+
+           row.add(1);
+
+           for(int j=1; j<i; j++){
+               row.add(prev_row.get(j-1)+prev_row.get(j));
+           }
+           row.add(1);
+           tri.add(row);
+       }
+       return tri;
+   }
+
+   // 5.b. Approach II
+
+
+   // 6.
 }
