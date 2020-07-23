294c6a854553b2c9ca7a9b1dd05d9ad2e8240381
Manish-rai21bit <manish.rai21bit@gmail.com>
2020-07-22 17:07:38 -0700

    contains duplicate - hash + sort method

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -90,5 +90,23 @@ class LeetCode {
    // 5.b. Approach II
 
 
-   // 6.
+   // 6. contains duplicate
+   // 2 methods: dictionary or sorting
+   public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numbers = new HashSet<>();
+        for (int i=0; i<nums.length; i++){
+          if (numbers.contains(nums[i])) return true;
+          numbers.add(nums[i]);
+        }
+        return false;
+    }
+
+    // Sorting the array methods
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=0; i<nums.length-1; i++){
+            if (nums[i]==nums[i+1]) return true;
+        }
+        return false;
+    }
 }
